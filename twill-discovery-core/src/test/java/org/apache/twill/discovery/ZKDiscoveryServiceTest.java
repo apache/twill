7f12d16f3f7ccc41ad8331b8e350c623b6b24bdf
Erick Tryzelaar <erickt@lab41.org>
2014-07-24 15:06:48 -0700

    (TWILL-95) Example version number fix and zookeeper test timeout - Update version number in the examples   - This fixes `mvn package` on a machine that hasn't had twill installed     on it. - bump up another zookeeper timeout
    Signed-off-by: Terence Yim <chtyim@apache.org>

--- twill-discovery-core/src/test/java/org/apache/twill/discovery/ZKDiscoveryServiceTest.java
+++ twill-discovery-core/src/test/java/org/apache/twill/discovery/ZKDiscoveryServiceTest.java
@@ -128,7 +128,7 @@ public class ZKDiscoveryServiceTest extends DiscoveryServiceTestBase {
     // Discover that registered host:port.
     Assert.assertTrue(waitTillExpected(1, discoverables));
 
-    KillZKSession.kill(zkClient.getZooKeeperSupplier().get(), zkServer.getConnectionStr(), 5000);
+    KillZKSession.kill(zkClient.getZooKeeperSupplier().get(), zkServer.getConnectionStr(), 10000);
 
     // Register one more endpoint to make sure state has been reflected after reconnection
     Cancellable cancellable2 = register(discoveryService, "test_expires", "localhost", 54322);
