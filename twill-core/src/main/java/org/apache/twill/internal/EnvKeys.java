6c7d32aa70facec92482a28334877057319d5263
yaojiefeng <yaojie@cask.co>
2016-09-22 14:35:04 -0700

    Use TwillRuntimeSpecification to carry runtime information instead of using environment
    This closes #13 on Github.
    
    Signed-off-by: Terence Yim <chtyim@apache.org>

--- twill-core/src/main/java/org/apache/twill/internal/EnvKeys.java
+++ twill-core/src/main/java/org/apache/twill/internal/EnvKeys.java
@@ -22,29 +22,17 @@ package org.apache.twill.internal;
  */
 public final class EnvKeys {
 
-  public static final String TWILL_ZK_CONNECT = "TWILL_ZK_CONNECT";
-  public static final String TWILL_APP_RUN_ID = "TWILL_APP_RUN_ID";
   public static final String TWILL_RUN_ID = "TWILL_RUN_ID";
   public static final String TWILL_INSTANCE_ID = "TWILL_INSTANCE_ID";
   public static final String TWILL_INSTANCE_COUNT = "TWILL_INSTANCE_COUNT";
-  public static final String TWILL_RESERVED_MEMORY_MB = "TWILL_RESERVED_MEMORY_MB";
-
-  public static final String TWILL_FS_USER = "TWILL_FS_USER";
 
   /**
    * Cluster filesystem directory for storing twill app related files.
    */
-  public static final String TWILL_APP_DIR = "TWILL_APP_DIR";
-
-  public static final String TWILL_APP_NAME = "TWILL_APP_NAME";
-
-  public static final String TWILL_APP_LOG_LEVEL = "TWILL_APP_LOG_LEVEL";
-
   public static final String TWILL_RUNNABLE_NAME = "TWILL_RUNNABLE_NAME";
 
   public static final String TWILL_LOG_KAFKA_ZK = "TWILL_LOG_KAFKA_ZK";
 
-  public static final String YARN_RM_SCHEDULER_ADDRESS = "YARN_RM_SCHEDULER_ADDRESS";
   public static final String YARN_APP_ID = "YARN_APP_ID";
   public static final String YARN_APP_ID_CLUSTER_TIME = "YARN_APP_ID_CLUSTER_TIME";
   public static final String YARN_APP_ID_STR = "YARN_APP_ID_STR";
@@ -52,6 +40,7 @@ public final class EnvKeys {
   public static final String YARN_CONTAINER_ID = "YARN_CONTAINER_ID";
   public static final String YARN_CONTAINER_HOST = "YARN_CONTAINER_HOST";
   public static final String YARN_CONTAINER_PORT = "YARN_CONTAINER_PORT";
+
   /**
    * Used to inform runnables of their resource usage.
    */
---
+++ twill-core/src/main/java/org/apache/twill/internal/TwillRuntimeSpecification.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.twill.internal;
+
+import org.apache.twill.api.RunId;
+import org.apache.twill.api.TwillApplication;
+import org.apache.twill.api.TwillSpecification;
+import org.apache.twill.api.logging.LogEntry;
+
+import java.net.URI;
+import javax.annotation.Nullable;
+
+/**
+ * Represents runtime specification of a {@link TwillApplication}.
+ */
+public class TwillRuntimeSpecification {
+
+  private final TwillSpecification twillSpecification;
+
+  private final String fsUser;
+  private final URI twillAppDir;
+  private final String zkConnectStr;
+  private final RunId twillRunId;
+  private final String twillAppName;
+  private final int reservedMemory;
+  private final String rmSchedulerAddr;
+  private final LogEntry.Level logLevel;
+
+  public TwillRuntimeSpecification(TwillSpecification twillSpecification, String fsUser, URI twillAppDir,
+                                   String zkConnectStr, RunId twillRunId, String twillAppName,
+                                   int reservedMemory, @Nullable String rmSchedulerAddr,
+                                   @Nullable LogEntry.Level logLevel) {
+    this.twillSpecification = twillSpecification;
+    this.fsUser = fsUser;
+    this.twillAppDir = twillAppDir;
+    this.zkConnectStr = zkConnectStr;
+    this.twillRunId = twillRunId;
+    this.twillAppName = twillAppName;
+    this.reservedMemory = reservedMemory;
+    this.rmSchedulerAddr = rmSchedulerAddr;
+    this.logLevel = logLevel;
+  }
+
+  public TwillSpecification getTwillSpecification() {
+    return twillSpecification;
+  }
+
+  public String getFsUser() {
+    return fsUser;
+  }
+
+  public URI getTwillAppDir() {
+    return twillAppDir;
+  }
+
+  public String getZkConnectStr() {
+    return zkConnectStr;
+  }
+
+  public RunId getTwillRunId() {
+    return twillRunId;
+  }
+
+  public String getTwillAppName() {
+    return twillAppName;
+  }
+
+  public int getReservedMemory() {
+    return reservedMemory;
+  }
+
+  @Nullable
+  public String getRmSchedulerAddr() {
+    return rmSchedulerAddr;
+  }
+
+  @Nullable
+  public LogEntry.Level getLogLevel() {
+    return logLevel;
+  }
+}
